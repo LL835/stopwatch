68783ac828a5709d0814bbbcc5f0c4ba8807860b
ll835 <loktinlo@gmail.com>
2022-11-27 10:55:58 +0000

    draft startTimer and updateDisplay functions
    write code to start the timer and update the display for the user

--- script.js
+++ script.js
@@ -1,18 +1,57 @@
-startButton = document.getElementById("start-button");
-pauseButton = document.getElementById("pause-button");
-resetButton = document.getElementById("reset-button");
-lapButton = document.getElementById("lap-button");
+const startButton = document.getElementById("start-button");
+const pauseButton = document.getElementById("pause-button");
+const resetButton = document.getElementById("reset-button");
+const lapButton = document.getElementById("lap-button");
+timer = document.getElementById("timer")
+
+let cs = 0;
+let secs = 0;
+let mins = 0;
+let hrs = 0;
+
 
 // EVENT LISTENERS
 startButton.addEventListener("click", () => {
-    // run code to start timer
+    setInterval(startTimer, 10);
 })
 pauseButton.addEventListener("click", () => {
     // run code to pause timer
 })
 resetButton.addEventListener("click", () => {
-    // // run code to reset timer
+    // run code to reset timer
 })
 lapButton.addEventListener("click", () => {
-    // // run code to record a lap
+    // run code to record a lap
 })
+
+// FUNCTIONS
+function startTimer(){
+    cs++;
+    if (cs === 100) {
+        cs = 0;
+        secs++;
+    };
+    if (secs === 60){
+        secs = 0;
+        mins++
+    }
+    if (mins === 60){
+        mins = 0;
+        hrs++
+    }
+    updateDisplay()
+}
+function updateDisplay(){
+    let csFormatted = cs;
+    let secsFormatted = secs;
+    let minsFormatted = mins;
+    let hrsFormatted = hrs;
+
+    if (cs < 10) csFormatted = "0" + cs;
+    if (secs < 10) secsFormatted = "0" + secs;
+    if (mins < 10) minsFormatted = "0" + mins;
+    if (hrs < 10) hrsFormatted = "0" + hrs;
+
+    timer.innerText = hrsFormatted + ":" + minsFormatted + ":" + secsFormatted + ":" + csFormatted;
+    
+}
