f17017adfd2b29081daabf221af02abea0335422
ll835 <loktinlo@gmail.com>
2022-11-27 09:55:57 +0000

    attach event listeners to stopwatch buttons

---
+++ script.js
@@ -0,0 +1,18 @@
+startButton = document.getElementById("start-button");
+pauseButton = document.getElementById("pause-button");
+resetButton = document.getElementById("reset-button");
+lapButton = document.getElementById("lap-button");
+
+// EVENT LISTENERS
+startButton.addEventListener("click", () => {
+    // run code to start timer
+})
+pauseButton.addEventListener("click", () => {
+    // run code to pause timer
+})
+resetButton.addEventListener("click", () => {
+    // // run code to reset timer
+})
+lapButton.addEventListener("click", () => {
+    // // run code to record a lap
+})
